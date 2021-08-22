feab18dde787ca59d481943d683de9a4c38f6fd2
Esa Jokinen <esa@esajokinen.net>
2021-08-22 12:00:18 +0300

    Add support for pipe input.

--- test-cache-enabler.py
+++ test-cache-enabler.py
@@ -6,6 +6,8 @@
 #
 # Usage: test-cache-enabler.py https://example.com [...]
 #
+# Also takes line break separated URLs from a pipe.
+#
 # Author : Esa Jokinen (oh2fih)
 # -----------------------------------------------------------
 
@@ -61,9 +63,17 @@ def getCacheTime(page):
 
 def usage():
     print("\nUsage: " + sys.argv[0] + " https://example.com [...]")
+    print("\nAlso takes line break separated URLs from a pipe.\n")
     exit(1)
 
 if __name__ == "__main__":
+    urllist = []
     if len(sys.argv) == 1:
-        usage()
-    main(sys.argv[1:])
+        if sys.stdin.isatty():
+            usage()
+    else:
+        urllist.extend(sys.argv[1:])
+    if not sys.stdin.isatty():
+        for line in sys.stdin:
+            urllist.append(line.strip('\n\r\t'))
+    main(urllist)
