d4a733890097e7b4c9f12efe001b3c6618b90beb
Esa Jokinen <58781154+oh2fih@users.noreply.github.com>
2021-12-16 14:17:23 +0200

    Enable running netcat-proxy.sh on background.

--- netcat-proxy.sh
+++ netcat-proxy.sh
@@ -25,12 +25,15 @@ clipipe=$(mktemp -u)
 mkfifo -m 600 "$clipipe" || exit 1
 
 # Launch two netcats for client and server connections.
-while true; do nc "$2" "$3" > "$srvpipe" < "$clipipe"; done &
+while true; do nc "$2" "$3" > "$srvpipe" < "$clipipe" ; done &
 TARGET_PID=$!
 while true; do nc -l -p "$1" < "$srvpipe" > "$clipipe" ; done &
 LISTENER_PID=$!
-
 printf "\n%s\n" "Proxying port $1 to target $2:$3. Stop with Ctrl-C." 
 
 # Cleanup after Ctrl-C.
-trap 'kill $TARGET_PID ; kill $LISTENER_PID ; rm "$srvpipe" "$clipipe"' INT HUP ; read -r -d '' _ </dev/tty
+trap 'kill $TARGET_PID ; kill $LISTENER_PID ; rm "$srvpipe" "$clipipe" ; exit' INT
+trap 'exit' TERM
+trap "kill 0" EXIT
+while true; do sleep 1; done
+
