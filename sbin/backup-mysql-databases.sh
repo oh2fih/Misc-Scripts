5e04bf845e808b139cd8791c2463db9aca3b3755
Esa Jokinen <esa@esajokinen.net>
2022-12-11 15:10:12 +0200

    add backup-mysql-databases.sh

---
+++ sbin/backup-mysql-databases.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# -----------------------------------------------------------
+# Backup all MySQL/MariaDB databases; dump & compress.
+# Overwrites older backups matching the same date pattern.
+# Recommended to be scheduled with a SystemD service & timer.
+#
+# Default settings can be changed with environment variables:
+#  ExcludeDatabases: databases to exclude, separated with '|'
+#  DatePattern: +FORMAT; see man date(1)
+#
+# Author : Esa Jokinen (oh2fih)
+# -----------------------------------------------------------
+
+DefaultExcludeDatabases="information_schema|performance_schema|mysql"
+DefaultDatePattern="%d"
+
+# Set defaults if environment variables not set.
+
+if [[ ! -v ExcludeDatabases ]]; then
+  ExcludeDatabases="$DefaultExcludeDatabases"
+fi
+
+if [[ ! -v DatePattern ]]; then
+  DatePattern=$DefaultDatePattern
+fi
+
+# Check for requirements.
+
+if ! command -v mysql > /dev/null 2>&1; then
+  echo "*** ERROR! This script requires mysql!"
+  exit 1
+fi
+
+if ! command -v mysqldump > /dev/null 2>&1; then
+  echo "*** ERROR! This script requires mysqldump!"
+  exit 1
+fi
+
+if ! command -v gzip > /dev/null 2>&1; then
+  echo "*** WARNING! Gzip not found; skipping compression."
+  compress=false
+else
+  compress=true
+fi
+
+date=$(date "+${DatePattern}")
+
+set -e
+
+# List existing databases not excluded.
+
+databases=$(
+  mysql -N -B -e "SHOW DATABASES;" \
+    | grep -E -v "$ExcludeDatabases"
+)
+
+echo "Working directory: $(pwd)"
+echo "Excluding databases: ${ExcludeDatabases}"
+echo "Date from pattern (${DatePattern}): ${date}"
+
+# Backup & compress.
+
+for db in $databases; do
+  echo "Dumping database: ${db}"
+  mysqldump --databases "$db" > "${date}-${db}.sql"
+  if [ "$compress" = true ]; then
+    echo "Compressing dump: ${date}-${db}.sql"
+    gzip -f "${date}-${db}.sql"
+  else
+    echo "Saved: ${date}-${db}.sql"
+  fi
+done
+
+echo "All backups successful."
